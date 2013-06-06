511f1b08c5587b96ad97be2263202aa97251d98a
kay <kay_s@live.ie>
2013-06-07 02:17:00 +0900

    県情報テストデータ追加

---
+++ test_data/pref.sql
@@ -0,0 +1,54 @@
+CREATE TABLE PREF (
+    PREF_NAME       VARCHAR(255)
+    , PREF_NUMBER   INT
+    , PREF_CAPITAL  VARCHAR(255)
+    , PRIMARY KEY(PREF_NAME)
+);
+
+INSERT INTO PREF VALUES('北海道', '1 ', '札幌');
+INSERT INTO PREF VALUES('青森県', '2 ', '青森');
+INSERT INTO PREF VALUES('岩手県', '3 ', '盛岡');
+INSERT INTO PREF VALUES('宮城県', '4 ', '仙台');
+INSERT INTO PREF VALUES('秋田県', '5 ', '秋田');
+INSERT INTO PREF VALUES('山形県', '6 ', '山形');
+INSERT INTO PREF VALUES('福島県', '7 ', '福島');
+INSERT INTO PREF VALUES('茨城県', '8 ', '水戸');
+INSERT INTO PREF VALUES('栃木県', '9 ', '宇都宮');
+INSERT INTO PREF VALUES('群馬県', '10 ', '前橋');
+INSERT INTO PREF VALUES('埼玉県', '11 ', 'さいたま');
+INSERT INTO PREF VALUES('千葉県', '12 ', '千葉');
+INSERT INTO PREF VALUES('東京都', '13 ', '東京');
+INSERT INTO PREF VALUES('神奈川県', '14 ', '横浜');
+INSERT INTO PREF VALUES('新潟県', '15 ', '新潟');
+INSERT INTO PREF VALUES('富山県', '16 ', '富山');
+INSERT INTO PREF VALUES('石川県', '17 ', '金沢');
+INSERT INTO PREF VALUES('福井県', '18 ', '福井');
+INSERT INTO PREF VALUES('山梨県', '19 ', '甲府');
+INSERT INTO PREF VALUES('長野県', '20 ', '長野');
+INSERT INTO PREF VALUES('岐阜県', '21 ', '岐阜');
+INSERT INTO PREF VALUES('静岡県', '22 ', '静岡');
+INSERT INTO PREF VALUES('愛知県', '23 ', '名古屋');
+INSERT INTO PREF VALUES('三重県', '24 ', '津');
+INSERT INTO PREF VALUES('滋賀県', '25 ', '大津');
+INSERT INTO PREF VALUES('京都府', '26 ', '京都');
+INSERT INTO PREF VALUES('大阪府', '27 ', '大阪');
+INSERT INTO PREF VALUES('兵庫県', '28 ', '神戸');
+INSERT INTO PREF VALUES('奈良県', '29 ', '奈良');
+INSERT INTO PREF VALUES('和歌山県', '30 ', '和歌山');
+INSERT INTO PREF VALUES('鳥取県', '31 ', '鳥取');
+INSERT INTO PREF VALUES('島根県', '32 ', '松江');
+INSERT INTO PREF VALUES('岡山県', '33 ', '岡山');
+INSERT INTO PREF VALUES('広島県', '34 ', '広島');
+INSERT INTO PREF VALUES('山口県', '35 ', '山口');
+INSERT INTO PREF VALUES('徳島県', '36 ', '徳島');
+INSERT INTO PREF VALUES('香川県', '37 ', '高松');
+INSERT INTO PREF VALUES('愛媛県', '38 ', '松山');
+INSERT INTO PREF VALUES('高知県', '39 ', '高知');
+INSERT INTO PREF VALUES('福岡県', '40 ', '福岡');
+INSERT INTO PREF VALUES('佐賀県', '41 ', '佐賀');
+INSERT INTO PREF VALUES('長崎県', '42 ', '長崎');
+INSERT INTO PREF VALUES('熊本県', '43 ', '熊本');
+INSERT INTO PREF VALUES('大分県', '44 ', '大分');
+INSERT INTO PREF VALUES('宮崎県', '45 ', '宮崎');
+INSERT INTO PREF VALUES('鹿児島県', '46 ', '鹿児島');
+INSERT INTO PREF VALUES('沖縄県', '47 ', '那覇');
